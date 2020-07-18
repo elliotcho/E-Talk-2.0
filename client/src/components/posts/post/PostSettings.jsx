7d17f0ef3f565c3028acfcf952bae6ffeff580f6
Yong <ycho97@uwo.ca>
2020-07-18 18:04:30 -0400

    todo: ellipsis dropdown backend

--- client/src/components/posts/post/PostSettings.jsx
+++ client/src/components/posts/post/PostSettings.jsx
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
 
 class PostSettings extends Component{
+    constructor(){
+        super();
+
+        this.state = {
+            clicked: false
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        const {clicked} = this.state;
+
+        this.setState({
+            clicked: !clicked
+        });
+    }
+
     render(){
+        const {clicked} = this.state;
+
+        const ellipsisStyle = (clicked)? {visibility: 'hidden'} :  {visibility: 'visible'};
+
         return(
-            <div className = 'post-settings'>
-                <i className ='fa fa-ellipsis-h'></i>
+            <div>
+                <div className = 'post-settings' onClick = {this.handleClick}>
+                    <i className ='fa fa-ellipsis-h' style = {ellipsisStyle}/>
+                </div>
             </div>
         )
     }
