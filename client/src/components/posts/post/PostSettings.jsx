657eee3e98e229ca7ac09edcf820d6080cdd4dc6
Yong <ycho97@uwo.ca>
2020-07-18 23:36:54 -0400

    ellipsis dropdown on posts done

--- client/src/components/posts/post/PostSettings.jsx
+++ client/src/components/posts/post/PostSettings.jsx
@@ -1,33 +1,31 @@
 import React, {Component} from 'react';
 
 class PostSettings extends Component{
-    constructor(){
-        super();
-
-        this.state = {
-            clicked: false
-        }
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(){
-        const {clicked} = this.state;
-
-        this.setState({
-            clicked: !clicked
-        });
-    }
-
     render(){
-        const {clicked} = this.state;
+        const {uid, ownerId, postId, deletePost} = this.props;
 
-        const ellipsisStyle = (clicked)? {visibility: 'hidden'} :  {visibility: 'visible'};
+        const ownerSettings = (
+            <div>
+                <div className = 'option' onClick = {() => {deletePost(postId)}}>
+                    <i className ='fas fa-trash-alt'></i>
+                    <span className ='ml-1'>Delete Post</span>
+                </div>
+
+                <div className = 'option'>
+                    <i className ='fas fa-edit'></i>
+                    <span className='ml-1'>Edit Post</span>
+                </div>
+            </div>
+        );
 
         return(
             <div>
-                <div className = 'post-settings' onClick = {this.handleClick}>
-                    <i className ='fa fa-ellipsis-h' style = {ellipsisStyle}/>
+                <div className = 'post-settings'>
+                    <i className ='fa fa-ellipsis-h'/>
+
+                    <div className ='dropdown-content'>
+                       {uid === ownerId? ownerSettings: null}
+                    </div>
                 </div>
             </div>
         )
