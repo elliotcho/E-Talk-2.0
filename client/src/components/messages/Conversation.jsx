7d66114007e694f9df303bbcc408def82c9ee56a
Yong <ycho97@uwo.ca>
2020-08-01 18:34:53 -0400

    some progress in designing

--- client/src/components/messages/Conversation.jsx
+++ client/src/components/messages/Conversation.jsx
@@ -5,12 +5,48 @@ class Conversation extends Component{
     render(){
         return(
             <div className ='convo'>
-                <header className ='convo-header'>
+                <header>
                     <div className ='chat-info'>
                         <img src={loading} className='chat-pic' alt='chat pic'/>
                         <h2>Gugsa Challa, Jeet Undhad</h2>
                     </div>
                 </header>
+
+                <section className ='chat-box'>
+                    <div className ='row no-gutters'>
+                        <div className='msg-container'>
+                            <div className ='msg msg-r my-1'>
+                                <div className='msg-content'>
+                                    Hello hello hello hello hello hello
+                                </div>
+
+                                <div className = 'read mx-1 my-1'>
+                                    <i className ='fa fa-check mt-1'/>
+                                    <span>September 30, 2019</span>
+                                </div>
+                            </div>
+
+                            <img src = {loading} alt ='profile pic'/>
+                        </div>
+                    </div>
+
+                    <div className ='row no-gutters'>
+                        <div className='msg-container'>
+                            <img src = {loading} alt ='profile pic'/>
+
+                            <div className ='msg msg-l my-1'>
+                                <div className='msg-content'>
+                                    Hello hello hello hello hello hello
+                                </div>
+
+                                <div className = 'read my-1 mx-1'>
+                                    <i className ='fa fa-check mt-1'/>
+                                    <span>September 30, 2019</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         )
     }
