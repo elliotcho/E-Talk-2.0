a8c7fa67aaf24caddd21d6056e96d729582ef3d7
Yong <ycho97@uwo.ca>
2020-08-05 17:02:01 -0400

    fixed bugs on compose msg backend

--- server/socket/messages.js
+++ server/socket/messages.js
@@ -3,20 +3,22 @@ const {User} = require('../dbschemas');
 exports.getContacts = async (data) =>{
     const {uid, name} = data;
 
-    let query = name.trim().toLowerCase();
+    let query = name.split(" ").join("").toLowerCase();
 
     const user = await User.findOne({_id: uid});
     const friends = await User.find({_id: {$in: user.friends}});
 
-    if(friends.length === 0){return [];}
+    if(friends.length === 0 || query === ''){
+        return [];
+    }
 
     let result = [];
 
     for(let i=0;i<friends.length; i++){
-        let friendFirstName = friends[i].firstName.trim().toLowerCase();
-        let friendLastName = friends[i].lastName.trim().toLowerCase();
+        let friendFirstName = friends[i].firstName.split(" ").join("").toLowerCase();
+        let friendLastName = friends[i].lastName.split(" ").join("").toLowerCase();
 
-        if(`${friendFirstName}${friendLastName}`.startsWith(query)){
+        if((friendFirstName + friendLastName).startsWith(query)){
             result.push(friends[i]);
         }
     }
