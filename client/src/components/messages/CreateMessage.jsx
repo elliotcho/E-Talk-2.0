8fc0c1a96b44dfb2a1c3c37985a6fd39ffd11939
Yong <ycho97@uwo.ca>
2020-08-06 02:10:22 -0400

    styling create message height limits/scroll

--- client/src/components/messages/CreateMessage.jsx
+++ client/src/components/messages/CreateMessage.jsx
@@ -1,15 +1,63 @@
 import React, {Component} from 'react';
 
 class CreateMessage extends Component{
+    constructor(){
+        super();
+
+        this.state = {
+            messsageContent: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.pressEnter = this.pressEnter.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({[e.target.id]: e.target.value});
+    }
+
+    pressEnter(e){
+        if(e.keyCode === 13 && e.shiftKey === false){
+            this.myMessageForm.dispatchEvent(new Event('submit'));
+        }
+
+        else{
+           this.myMessage.style.height = "";
+           this.myMessage.style.height = this.myMessage.scrollHeight + 'px';
+        }
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+
+        let {messageContent} = this.state;
+        let n = messageContent.length;
+
+        if(messageContent.trim() === ""){
+            messageContent = messageContent.substring(0, n-1);
+            
+            this.setState({messageContent});
+
+            return;
+        }
+    }
+
     render(){
+        const {messageContent} = this.state;
+
         return(
             <div className ='create-msg'>
-                <form ref = {ele => this.myMessageForm = ele}>
+                <form ref = {ele => this.myMessageForm = ele} onSubmit={this.handleSubmit}>
                     <textarea
+                        id = 'messageContent'
                         className ='form-control'
                         rows ='1'
                         ref = {ele =>this.myMessage = ele}
                         placeholder ='Type a message...'
+                        onKeyUp = {this.pressEnter}
+                        onChange = {this.handleChange}
+                        value = {messageContent}
                     />
 
                     <label>
