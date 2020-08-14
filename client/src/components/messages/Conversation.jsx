a3e01683aa832be079ad361a70b9dc76cc3e7844
Yong <ycho97@uwo.ca>
2020-08-14 17:53:29 -0400

    played around with color scheme

--- client/src/components/messages/Conversation.jsx
+++ client/src/components/messages/Conversation.jsx
@@ -113,7 +113,7 @@ class Conversation extends Component{
                                 <img key={0} src={chatPics[0]} alt='profile-pic' className='chat-pic'/>,
                                 <img  key={1} src={chatPics[1]} alt ='profile-pic' className='chat-pic second-pic'/>
                             ])
-                            :(<img src={chatPics.length? chatPics[0]: loading} alt='profile-pic' className='chat-pic'/>)
+                            :(<img src={chatPics.length? chatPics[0]: loading} alt='profile-pic' className='chat-pic second-pic'/>)
                         }
 
 
