8cd30bf7842f11823f99796682588d1a30a210bc
Yong <ycho97@uwo.ca>
2020-09-21 21:25:42 -0400

    validate name on sign up in auth actions

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -1,11 +1,11 @@
 import * as types from '../constants/actionTypes';
 import axios from 'axios';
 
+const config={headers: {'content-type': 'application/json'}};
+
 //user login: updates uid in global state
 export const login = (credentials) =>{
     return async (dispatch) =>{
-        const config={headers: {'content-type': 'application/json'}};
-
         const response = await axios.post('http://localhost:5000/users/login', {...credentials}, config)
         const {msg, uid} = response.data;
     
@@ -25,7 +25,12 @@ export const login = (credentials) =>{
 //user sign up: updates uid in global state
 export const signUp = (credentials) =>{
     return async (dispatch) =>{
-        const config={headers: {'content-type': 'application/json'}};
+        const {firstName, lastName} = credentials;
+
+        if(!verifyName(firstName) || !verifyName(lastName)){
+            alert("Invalid name: First name and last name should only contain letters!");
+            return;
+        }
 
         const response = await axios.post('http://localhost:5000/users/signup', {...credentials}, config);
         const {msg, uid} = response.data;
@@ -41,4 +46,17 @@ export const signUp = (credentials) =>{
             alert(msg);
         }
     }
+}
+
+const verifyName = (name) => {
+    let valid = true;
+
+    for(let i=0;i<name.length;i++){
+        if(name[i].toLowerCase() === name[i].toUpperCase()){
+            valid = false;
+            break;
+        }
+    }
+
+    return valid;
 }
